fd8618170be9d3f33a7c1aa2d5fa896cda372a77
renzgans <108325453+renzgans@users.noreply.github.com>
2022-06-30 17:20:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@RenzStoreDL'
+  var sticker_author = 'Renz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Remz'
+global.wm = '© Renz'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
